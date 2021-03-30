6e87f848583b9dbd16395589889d936a83fcb9e8
Leticia <74209482+sampaioleticia@users.noreply.github.com>
2021-03-30 18:02:55 -0300

    Update alturaArvore.hs

--- alturaArvore.hs
+++ alturaArvore.hs
@@ -1,5 +1,8 @@
 data Tree a = Leaf a 
             | Branch (Tree a) (Tree a)
-altura :: Tree a → Int
-altura Leaf _ = 0
-altura (Branch leftNode _ rightNode _ ) = 1 + max (altura leftNode _ ) (altura rightNode _ )
+     
+altura :: Tree a -> Int
+altura Leaf = 0
+altura (Branch _ a b) = 1 + max (height a) (height b)
+
+
