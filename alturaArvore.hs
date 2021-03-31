0685b2d47465edd4df7aef03c429fdce765bbf74
Leticia <74209482+sampaioleticia@users.noreply.github.com>
2021-03-30 23:02:27 -0300

    Update alturaArvore.hs

--- alturaArvore.hs
+++ alturaArvore.hs
@@ -2,8 +2,5 @@ data Tree a = Leaf a
             | Branch (Tree a) (Tree a)
      
 altura :: Tree a -> Int
-altura Leaf a = 1
-altura (Branch a esquerda direita) = if altura esquerda > altura direita then 
-   (altura esquerda + 1)
-   else 
-   (altura direita + 1)
+altura (Leaf x) = 1
+altura (Branch x esquerda direita) = max (altura esquerda) (altura direita) + 1
