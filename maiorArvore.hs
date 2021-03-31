41a34e0c0b5d63b39613399a9baa4201e934af7e
Leticia <74209482+sampaioleticia@users.noreply.github.com>
2021-03-30 22:50:18 -0300

    Update maiorArvore.hs

--- maiorArvore.hs
+++ maiorArvore.hs
@@ -6,9 +6,6 @@ menorValor [x] = x
 menorValor (x:y:xs) |x < y = menorValor (y:xs)
                     |x > y = menorValor (x:xs)
                     |x == y = menorValor (x:xs)
-                                 
-pegarValor Leaf a = a
-pegarValor (Branch a esquerda direita) = pegarValor esquerda ++ a ++ pegarValor direita
 
 maior :: Ord a => Tree a -> a
-maior a = menorValor (pegarValor a)
+maior a = menorValor (esquerda ++ a ++ direita)
